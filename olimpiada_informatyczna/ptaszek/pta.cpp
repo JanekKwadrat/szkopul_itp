7dd02ee5bbd292d40b9286a5a43b87ceeb66560f
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-02-06 17:24:12 +0100

    finished rmv (hopefully), added destructor ~Tree

--- olimpiada_informatyczna/ptaszek/pta.cpp
+++ olimpiada_informatyczna/ptaszek/pta.cpp
@@ -33,10 +33,23 @@ class Tree {
         ptr1 = nullptr;
     }
 
+    ~Tree(){
+        if(ptr0 != nullptr) delete ptr0;
+        if(ptr1 != nullptr) delete ptr1;
+    }
+
     void add(int _itr, int _hgt){
         Tree ** ptr_a = _hgt < hgt ? &ptr0 : &ptr1;
         if(*ptr_a == nullptr) *ptr_a = new Tree(_itr, _hgt);
         else (*ptr_a)->add(_itr, _hgt);
+    }
+
+    bool find(int _itr, int _hgt){
+        if(itr == _itr && hgt == _hgt) return true;
+        bool ans = false;
+        if(_hgt < hgt) ans = ans || (ptr0 != nullptr && ptr0->find(_itr, _hgt));
+        else ans = ans || (ptr1 != nullptr && ptr1->find(_itr, _hgt));
+        return ans;
     }
 
     void rmv(int _itr, int _hgt){
@@ -46,6 +59,25 @@ class Tree {
             else if(ptr1->ptr0 == nullptr){
                 ptr1->ptr0 = ptr0;
                 *this = *ptr1;
+            }
+            else {
+                Tree * prev = ptr1;
+                Tree * ptr_t = ptr1->ptr0;
+
+                while(true){
+                    if(ptr_t->ptr0 == nullptr) break;
+                    prev = ptr_t;
+                    ptr_t = ptr_t->ptr0;
+                }
+
+                itr = ptr_t->itr;
+                hgt = ptr_t->hgt;
+
+                if(ptr_t->ptr1 != nullptr) ptr_t = ptr_t->ptr1;
+                else {
+                    delete ptr_t;
+                    prev->ptr0 = nullptr;
+                }
             }
         }
         else {
@@ -58,7 +90,17 @@ class Tree {
 };
 
 int main(){
-    Tree * my_tree = new Tree(8, 1);
-    std::cout << "Hello, world!\n";
+    Tree * my_tree = new Tree(0, 0);
+    my_tree->add(6, 7);
+    my_tree->add(2, 3);
+    my_tree->add(0, 4);
+    my_tree->add(1, 6);
+    my_tree->add(5, 2);
+    my_tree->add(3, 1);
+    std::cout << my_tree->find(6, 7) << "\n";
+    std::cout << my_tree->find(1, 6) << "\n";
+    std::cout << !my_tree->find(4, 2) << "\n";
+    my_tree->rmv(1, 6);
+    std::cout << !my_tree->find(1, 6) << "\n";
     return 0;
 }
