091e41beab65855074569143e16683ab76dbb792
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-02-06 16:51:52 +0100

    some algo added

--- olimpiada_informatyczna/ptaszek/pta.cpp
+++ olimpiada_informatyczna/ptaszek/pta.cpp
@@ -3,54 +3,62 @@
  * Jan Zakrzewski, rozwiazanie
  * 
  * Pomysl: dynamiczne drzewo przedzialowe z programowaniem dynamicznym
- * (przechowujace wyniki, a segregowane wedlug wysokosci)
+ * (przechowujace wynikiki, a segregowane wedlug wysokosci)
  */
 
 #include <iostream>
 
+inline int min(int x, int y){
+    return x < y ? x : y;
+}
+
 class Tree {
-    int hold; // value held in node
+    int itr; // index of the forest-tree in whole sequence
+    int hgt; // height of the forest-tree
+    //int ans; // answer to the problem when starting at itr
+    
+    //int all; // minimum of answers over this node and all its children
+
     int size; // number of nodes - this one and all its children
     Tree * ptr0; // pointer to left subtree
     Tree * ptr1; // pointer to right subtree
 
     public:
     
-    Tree(int x){
-        hold = x;
+    Tree(int _itr, int _hgt){
+        itr = _itr;
+        hgt = _hgt;
         size = 1;
         ptr0 = nullptr;
         ptr1 = nullptr;
     }
 
-    int add(int x){
-        int wh = 0;
-        if(x < hold){
-            if(ptr0 == nullptr) ptr0 = new Tree(x);
-            else wh += ptr0->add(x);
+    void add(int _itr, int _hgt){
+        Tree ** ptr_a = _hgt < hgt ? &ptr0 : &ptr1;
+        if(*ptr_a == nullptr) *ptr_a = new Tree(_itr, _hgt);
+        else (*ptr_a)->add(_itr, _hgt);
+    }
+
+    void rmv(int _itr, int _hgt){
+        if(_hgt == hgt && _itr == itr){
+            if(ptr0 == nullptr && ptr1 == nullptr) size = 0;
+            else if(ptr1 == nullptr) *this = *ptr0;
+            else if(ptr1->ptr0 == nullptr){
+                ptr1->ptr0 = ptr0;
+                *this = *ptr1;
+            }
         }
         else {
-            if(ptr1 == nullptr) ptr1 = new Tree(x);
-            else wh += ptr1->add(x);
-            if(ptr0 != nullptr)
-                wh += ptr0->size;
-            ++wh;
+            if(_hgt < hgt) ptr0->rmv(_itr, _hgt);
+            else ptr1->rmv(_itr, _hgt);
         }
-        ++size;
-        return wh;
+        if(ptr0 != nullptr && ptr0->size == 0) ptr0 = nullptr;
+        if(ptr1 != nullptr && ptr1->size == 0) ptr1 = nullptr;
     }
 };
 
 int main(){
-    Tree * my_tree = new Tree(8);
-    my_tree->add(5);
-    my_tree->add(10);
-    my_tree->add(9);
-    my_tree->add(6);
-    my_tree->add(4);
-    my_tree->add(11);
-    std::cout << my_tree->add(7) << "\n";
-    std::cout << my_tree->add(12) << "\n";
+    Tree * my_tree = new Tree(8, 1);
     std::cout << "Hello, world!\n";
     return 0;
 }
