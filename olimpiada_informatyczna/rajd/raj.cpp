c62ddbd9dd358a842f5fc9ee91f8922f4ef0a786
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-02-07 12:17:34 +0100

    first commit of rajd.cpp

---
+++ olimpiada_informatyczna/rajd/raj.cpp
@@ -0,0 +1,17 @@
+/* 
+ * Zadanie: Rajd (XXI OI, etap drugi)
+ * Jan Zakrzewski, rozwiazanie
+ * 
+ * Pomysly: przyporzadkowac kazdemu skrzyzowaniu ile je poprzedza i ile jest po nim,
+ * czyli jakas pare [a|b].
+ * Jak policzyc ile jest objazdow danego skrzyzowania? Moze sprobowac przy liczeniu par [a|b] liczyc druga najlepsza trase?
+ */
+
+#include <iostream>
+
+int main() {
+
+    
+
+    return 0;
+}
