a656307b0a53da12f235eceed8a65916afc7c3d0
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-02-19 14:06:48 +0100

    modified prime_multiples, now works

--- cses/prime_multiples/pri.cpp
+++ cses/prime_multiples/pri.cpp
@@ -9,6 +9,8 @@ int p;
 ull primes[maxP];
 ull answer = 0;
 
+int log(ull);
+
 int main() {
 
     std::cin >> n >> p;
@@ -20,12 +22,23 @@ int main() {
         for(int k = 0; k < p; ++k) {
             if((itr >> k) % 2 == 0) continue;
             sgn *= -1;
+            if(log(nmb) + log(primes[k]) >= 60) nmb = 0;
             nmb *= primes[k];
         }
-        answer += sgn * (n / nmb);
+        if(nmb) answer += sgn * (n / nmb);
     }
 
     std::cout << answer << "\n";
 
     return 0;
+}
+
+int log(ull x) {
+    int ans = 0;
+    while(x) {
+        x /= 2;
+        ++ans;
+    }
+    --ans;
+    return ans;
 }
