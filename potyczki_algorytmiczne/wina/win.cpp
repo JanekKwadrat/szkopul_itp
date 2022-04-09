07cb10969456df389ee66c692c5dfd3ebd898bbb
Jan Zakrzewski <janek.kwadrat@gmail.com>
2022-04-09 21:24:27 +0200

    added solution to Potyczki Algorytmiczne 2019 - Wina [B]

---
+++ potyczki_algorytmiczne/wina/win.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+
+constexpr int max_n = 2005;
+
+int n, k;   
+int butelka;
+int najlepsza = 3000;
+int koszt;
+
+int main() {
+
+    std::cin >> n >> k;
+    for(int i = 1; i <= n; ++i) {
+        for(int j = 1; j <= i; ++j) {
+            std::cin >> butelka;
+            koszt = j * (i - j + 1);
+            if(koszt > k) continue;
+            if(butelka < najlepsza) najlepsza = butelka;
+        }
+    }
+    std::cout << najlepsza << "\n";
+
+    return 0;
+}
