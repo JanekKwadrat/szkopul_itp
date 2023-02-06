0af0e209bab3eea2b66547f111316c8c8a37bf57
Jan Zakrzewski <janek.kwadrat@gmail.com>
2023-02-06 13:49:51 +0100

    Rozwiązałem zadanie `Bomberman (bom)' na 100 punktów

--- olimpiada_informatyczna/bomberman/bom.cpp
+++ olimpiada_informatyczna/bomberman/bom.cpp
@@ -1,255 +1,265 @@
-#include <iostream>
-#include <iomanip>
-#include <queue>
-#include <tuple>
+/* Jan Zakrzewski
+   XXIX Olimpiada Informatyczna, Etap II
+   Zadanie `Bomberman (bom)' */
 
-const int max_n = 1020;
-const int infty = 1e7;
+#include <bits/stdc++.h>
+using namespace std;
+
+constexpr int N = 1100;
+constexpr int INF = 1e9;
+
 int n;
-char board[max_n][max_n];
-int dist[max_n][max_n];
-int distoth[max_n][max_n];
-int Px, Py, Kx, Ky;
-std::queue<std::pair<int, int>> wave;
+char board[N][N];
+pair<int, int> A, B;
 
-int min_hor_val[max_n][max_n];
-int min_hor_itr[max_n][max_n];
+int dist_A[N][N];
+int dist_A_hor[N][N];
+int dist_A_ver[N][N];
 
-int min_ver_val[max_n][max_n];
-int min_ver_itr[max_n][max_n];
+int dist_B[N][N];
+int dist_B_hor[N][N];
+int dist_B_ver[N][N];
 
-int minoth_hor_val[max_n][max_n];
-int minoth_hor_itr[max_n][max_n];
+constexpr int NOR = 1, HOR = 2, VER = 3;
 
-int minoth_ver_val[max_n][max_n];
-int minoth_ver_itr[max_n][max_n];
+queue<pair<pair<int, int>, int>> my_queue;
+queue<pair<int, int>> my_queue1;
 
-int answer = infty;
+vector<pair<int, int>> D = {{0, 1}, {1, 0}, {0, -1}, {-1, 0}};
+vector<pair<int, int>> D_hor = {{1, 0}, {-1, 0}};
+vector<pair<int, int>> D_ver = {{0, 1}, {0, -1}};
+
+int ans = INF;
+pair<int, int> bomb;
+string path;
 
 int main() {
-    std::cin >> n;
-    for(int y = 0; y < n; ++y)
-        std::cin >> board[y];
-    
-    for(int y = 0; y < n; ++y) for(int x = 0; x < n; ++x) {
-        if(board[y][x] == 'P') Px = x, Py = y;
-        if(board[y][x] == 'K') Kx = x, Ky = y;
-    }
 
-    for(int y = 0; y < n; ++y) for(int x = 0; x < n; ++x) dist[y][x] = infty;
+    ios_base::sync_with_stdio(0);
+    cin.tie(0), cout.tie(0);
 
-    wave.push(std::pair<int, int>(Py, Px));
-    dist[Py][Px] = 0;
-    while(!wave.empty()) {
-        int y = wave.front().first;
-        int x = wave.front().second;
-        wave.pop();
-
-        if(board[y][x] == 'X' || board[y][x] == '#') continue;
-
-        if(y+1 < n && dist[y][x] + 1 < dist[y+1][x]) {
-            dist[y+1][x] = dist[y][x] + 1;
-            wave.push(std::pair<int, int>(y+1, x));
-        }
-
-        if(y-1 >= 0 && dist[y][x] + 1 < dist[y-1][x]) {
-            dist[y-1][x] = dist[y][x] + 1;
-            wave.push(std::pair<int, int>(y-1, x));
-        }
-        
-        if(x+1 < n && dist[y][x] + 1 < dist[y][x+1]) {
-            dist[y][x+1] = dist[y][x] + 1;
-            wave.push(std::pair<int, int>(y, x+1));
-        }
-                
-        if(x-1 >= 0 && dist[y][x] + 1 < dist[y][x-1]) {
-            dist[y][x-1] = dist[y][x] + 1;
-            wave.push(std::pair<int, int>(y, x-1));
+    cin >> n;
+    for(int i = 1; i <= n; ++i) {
+        char str[N];
+        cin >> str+1;
+        for(int j = 1; j <= n; ++j) {
+            board[i][j] = str[j];
+            if(board[i][j] == 'P') A = make_pair(i, j);
+            if(board[i][j] == 'K') B = make_pair(i, j);
         }
     }
 
-    for(int y = 0; y < n; ++y) {
-        int val, itr;
-        val = infty;
-        itr = -1;
-        for(int x = 0; x < n; ++x) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(dist[y][x] < val) {
-                    val = dist[y][x];
-                    itr = x;
-                }
-                min_hor_val[y][x] = val;
-                min_hor_itr[y][x] = itr;
-            }
-        }
-        val = infty;
-        itr = -1;
-        for(int x = n-1; x >= 0; --x) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(min_hor_val[y][x] < val) {
-                    val = min_hor_val[y][x];
-                    itr = min_hor_itr[y][x];
-                }
-                min_hor_val[y][x] = val;
-                min_hor_itr[y][x] = itr;
-            }
-        }
+    for(int i = 0; i <= n+1; ++i) {
+        board[0][i] = 'X';
+        board[i][0] = 'X';
+        board[n+1][i] = 'X';
+        board[i][n+1] = 'X';
     }
 
-    for(int x = 0; x < n; ++x) {
-        int val, itr;
-        val = infty;
-        itr = -1;
-        for(int y = 0; y < n; ++y) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(dist[y][x] < val) {
-                    val = dist[y][x];
-                    itr = y;
-                }
-                min_ver_val[y][x] = val;
-                min_ver_itr[y][x] = itr;
-            }
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j) {
+            dist_A[i][j] = INF;
+            dist_A_hor[i][j] = INF;
+            dist_A_ver[i][j] = INF;
         }
-        val = infty;
-        itr = -1;
-        for(int y = n-1; y >= 0; --y) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(min_hor_val[y][x] < val) {
-                    val = min_ver_val[y][x];
-                    itr = min_ver_itr[y][x];
+    dist_A[A.first][A.second] = 0;
+    dist_A_hor[A.first][A.second] = 0;
+    dist_A_ver[A.first][A.second] = 0;
+    my_queue.push(make_pair(A, NOR));
+    my_queue.push(make_pair(A, HOR));
+    my_queue.push(make_pair(A, VER));
+    //int S = 50;
+    while(my_queue.size() > 0) {
+        //S--;
+        //if(S == 0) break;
+        pair<int, int> p = my_queue.front().first;
+        int t = my_queue.front().second;
+        my_queue.pop();
+        //cout << p.first << " " << p.second << " " << t << "\n";
+        if(t == NOR) {
+            //cout << dist_A[p.first][p.second] << "\n";
+            for(pair<int, int> d : D) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(board[q.first][q.second] != '#') {
+                    if(dist_A[q.first][q.second] < INF) break;
+                    dist_A[q.first][q.second] = dist_A[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, NOR));
+                    break;
                 }
-                min_ver_val[y][x] = val;
-                min_ver_itr[y][x] = itr;
+                while(true) {
+                    if(dist_A_hor[q.first][q.second] < INF) break;
+                    dist_A_hor[q.first][q.second] = dist_A[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, HOR));
+                    break;
+                }
+                while(true) {
+                    if(dist_A_ver[q.first][q.second] < INF) break;
+                    dist_A_ver[q.first][q.second] = dist_A[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, VER));
+                    break;
+                }
+            }
+        } else if(t == HOR) {
+            //cout << dist_A_hor[p.first][p.second] << "\n";
+            for(pair<int, int> d : D_hor) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(true) {
+                    if(dist_A_hor[q.first][q.second] < INF) break;
+                    dist_A_hor[q.first][q.second] = dist_A_hor[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, HOR));
+                    break;
+                }
+            }
+        } else if(t == VER) {
+            //cout << dist_A_ver[p.first][p.second] << "\n";
+            for(pair<int, int> d : D_ver) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(true) {
+                    if(dist_A_ver[q.first][q.second] < INF) break;
+                    dist_A_ver[q.first][q.second] = dist_A_ver[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, VER));
+                    break;
+                }
             }
         }
     }
 
-    for(int y = 0; y < n; ++y) for(int x = 0; x < n; ++x) distoth[y][x] = infty;
-
-    wave.push(std::pair<int, int>(Ky, Kx));
-    distoth[Ky][Kx] = 0;
-    while(!wave.empty()) {
-        int y = wave.front().first;
-        int x = wave.front().second;
-        wave.pop();
-
-        if(board[y][x] == 'X' || board[y][x] == '#') continue;
-
-        if(y+1 < n && distoth[y][x] + 1 < distoth[y+1][x]) {
-            distoth[y+1][x] = distoth[y][x] + 1;
-            wave.push(std::pair<int, int>(y+1, x));
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j) {
+            dist_B[i][j] = INF;
+            dist_B_hor[i][j] = INF;
+            dist_B_ver[i][j] = INF;
         }
-
-        if(y-1 >= 0 && distoth[y][x] + 1 < distoth[y-1][x]) {
-            distoth[y-1][x] = distoth[y][x] + 1;
-            wave.push(std::pair<int, int>(y-1, x));
-        }
-        
-        if(x+1 < n && distoth[y][x] + 1 < distoth[y][x+1]) {
-            distoth[y][x+1] = distoth[y][x] + 1;
-            wave.push(std::pair<int, int>(y, x+1));
-        }
-                
-        if(x-1 >= 0 && distoth[y][x] + 1 < distoth[y][x-1]) {
-            distoth[y][x-1] = distoth[y][x] + 1;
-            wave.push(std::pair<int, int>(y, x-1));
-        }
-    }
-
-    for(int y = 0; y < n; ++y) {
-        int val, itr;
-        val = infty;
-        itr = -1;
-        for(int x = 0; x < n; ++x) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(distoth[y][x] < val) {
-                    val = distoth[y][x];
-                    itr = x;
+    dist_B[B.first][B.second] = 0;
+    dist_B_hor[B.first][B.second] = 0;
+    dist_B_ver[B.first][B.second] = 0;
+    my_queue.push(make_pair(B, NOR));
+    my_queue.push(make_pair(B, HOR));
+    my_queue.push(make_pair(B, VER));
+    while(my_queue.size() > 0) {
+        pair<int, int> p = my_queue.front().first;
+        int t = my_queue.front().second;
+        my_queue.pop();
+        if(t == NOR) {
+            for(pair<int, int> d : D) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(board[q.first][q.second] != '#') {
+                    if(dist_B[q.first][q.second] < INF) break;
+                    dist_B[q.first][q.second] = dist_B[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, NOR));
+                    break;
                 }
-                minoth_hor_val[y][x] = val;
-                minoth_hor_itr[y][x] = itr;
+                while(true) {
+                    if(dist_B_hor[q.first][q.second] < INF) break;
+                    dist_B_hor[q.first][q.second] = dist_B[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, HOR));
+                    break;
+                }
+                while(true) {
+                    if(dist_B_ver[q.first][q.second] < INF) break;
+                    dist_B_ver[q.first][q.second] = dist_B[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, VER));
+                    break;
+                }
             }
-        }
-        val = infty;
-        itr = -1;
-        for(int x = n-1; x >= 0; --x) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(minoth_hor_val[y][x] < val) {
-                    val = minoth_hor_val[y][x];
-                    itr = minoth_hor_itr[y][x];
+        } else if(t == HOR) {
+            for(pair<int, int> d : D_hor) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(true) {
+                    if(dist_B_hor[q.first][q.second] < INF) break;
+                    dist_B_hor[q.first][q.second] = dist_B_hor[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, HOR));
+                    break;
                 }
-                minoth_hor_val[y][x] = val;
-                minoth_hor_itr[y][x] = itr;
             }
-        }
-    }
-
-    for(int x = 0; x < n; ++x) {
-        int val, itr;
-        val = infty;
-        itr = -1;
-        for(int y = 0; y < n; ++y) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(distoth[y][x] < val) {
-                    val = distoth[y][x];
-                    itr = y;
+        } else if(t == VER) {
+            for(pair<int, int> d : D_ver) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                while(true) {
+                    if(dist_B_ver[q.first][q.second] < INF) break;
+                    dist_B_ver[q.first][q.second] = dist_B_ver[p.first][p.second] + 1;
+                    my_queue.push(make_pair(q, VER));
+                    break;
                 }
-                minoth_ver_val[y][x] = val;
-                minoth_ver_itr[y][x] = itr;
-            }
-        }
-        val = infty;
-        itr = -1;
-        for(int y = n-1; y >= 0; --y) {
-            if(board[y][x] == 'X') val = infty;
-            else {
-                if(minoth_hor_val[y][x] < val) {
-                    val = minoth_ver_val[y][x];
-                    itr = minoth_ver_itr[y][x];
-                }
-                minoth_ver_val[y][x] = val;
-                minoth_ver_itr[y][x] = itr;
             }
         }
     }
 
-    for(int by = 0; by < n; ++by) for(int bx = 0; bx < n; ++bx) {
-        if(board[by][bx] == 'X') continue;
-        if(!answer) std::cout << by << " " << bx << "\n";
-        if(min_hor_itr[bx][by] == -1 || minoth_hor_itr[bx][by] == -1 || min_ver_itr[bx][by] == -1 || minoth_ver_itr[bx][by] == -1) continue;
-        answer = std::min(answer, dist[by][min_hor_itr[by][bx]] + abs(min_hor_itr[by][bx] - minoth_hor_itr[by][bx]) + distoth[by][minoth_hor_itr[by][bx]]);
-        answer = std::min(answer, dist[min_ver_itr[by][bx]][bx] + abs(min_ver_itr[by][bx] - minoth_ver_itr[by][bx]) + distoth[minoth_ver_itr[by][bx]][bx]);
-        answer = std::min(answer, dist[by][min_hor_itr[by][bx]] + abs(bx - min_hor_itr[by][bx]) + abs(by - minoth_ver_itr[by][bx]) + distoth[minoth_ver_itr[by][bx]][bx]);
-        answer = std::min(answer, dist[min_ver_itr[by][bx]][bx] + abs(by - min_ver_itr[by][bx]) + abs(bx - minoth_hor_itr[by][bx]) + distoth[by][minoth_hor_itr[by][bx]]);
-    }
-    
-    if(answer == infty) std::cout << "NIE\n";
-    else std::cout << answer << "\n";
+    for(int i = 1; i <= n; ++i)
+        for(int j = 1; j <= n; ++j) {
+            if(dist_A_hor[i][j] + dist_B_ver[i][j] < ans) {
+                ans = dist_A_hor[i][j] + dist_B_ver[i][j];
+                bomb = make_pair(i, j);
+            }
+            if(dist_A_ver[i][j] + dist_B_hor[i][j] < ans) {
+                ans = dist_A_ver[i][j] + dist_B_hor[i][j];
+                bomb = make_pair(i, j);
+            }
+        }
 
-    /*for(int y = 0; y < n; ++y) {
-        for(int x = 0; x < n; ++x)
-            std::cout << std::setw(7) << (board[y][x] == 'X' ? -1 : dist[y][x]) << " ";
-        std::cout << "\n";
+    if(ans == INF) cout << "NIE\n";
+    else {
+        cout << ans << "\n";
+        cout << bomb.first << " " << bomb.second << "\n";
+
+        // trzeba wybuchnąć bombę
+        int x, y;
+        x = bomb.first, y = bomb.second; while(board[x][y] != 'X') { board[x][y] = '.'; x--; }
+        x = bomb.first, y = bomb.second; while(board[x][y] != 'X') { board[x][y] = '.'; x++; }
+        x = bomb.first, y = bomb.second; while(board[x][y] != 'X') { board[x][y] = '.'; y--; }
+        x = bomb.first, y = bomb.second; while(board[x][y] != 'X') { board[x][y] = '.'; y++; }
+
+        for(int i = 0; i <= n+1; ++i)
+            for(int j = 0; j <= n+1; ++j)
+                dist_B[i][j] = INF;
+        dist_B[B.first][B.second] = 0;
+        my_queue1.push(make_pair(B.first, B.second));
+
+        while(my_queue1.size() > 0) {
+            pair<int, int> p = my_queue1.front();
+            my_queue1.pop();
+            for(pair<int, int> d : D) {
+                pair<int, int> q = make_pair(p.first + d.first, p.second + d.second);
+                if(board[q.first][q.second] == 'X') continue;
+                if(board[q.first][q.second] == '#') continue;
+                if(dist_B[q.first][q.second] < INF) continue;
+                dist_B[q.first][q.second] = dist_B[p.first][p.second] + 1;
+                my_queue1.push(q);        
+            }
+        }
+
+        path = "";
+        pair<int, int> p = A;
+        while(p != B) {
+                 if(dist_B[p.first][p.second-1] < dist_B[p.first][p.second]) { path += "L"; p.second--; }
+            else if(dist_B[p.first][p.second+1] < dist_B[p.first][p.second]) { path += "P"; p.second++; }
+            else if(dist_B[p.first-1][p.second] < dist_B[p.first][p.second]) { path += "G"; p.first--; }
+            else if(dist_B[p.first+1][p.second] < dist_B[p.first][p.second]) { path += "D"; p.first++; }
+        }
+
+        cout << path << "\n";
     }
-    std::cout << "\n";
-    for(int y = 0; y < n; ++y) {
-        for(int x = 0; x < n; ++x)
-            std::cout << std::setw(7) << (board[y][x] == 'X' ? -1 : min_hor_val[y][x]) << " ";
-        std::cout << "\n";
-    }
-    std::cout << "\n";
-    for(int y = 0; y < n; ++y) {
-        for(int x = 0; x < n; ++x)
-            std::cout << std::setw(7) << (board[y][x] == 'X' ? -1 : min_hor_itr[y][x]) << " ";
-        std::cout << "\n";
-    }*/
 
     return 0;
 }
+
+/*
+10
+P........X
+........X.
+.......X..
+......X...
+.....X....
+....X.....
+...X......
+..X.......
+.X........
+X.........
+
+*/
