31c04feb1c488283db8cd38ca22493d407822510
Jan Zakrzewski <janek.kwadrat@gmail.com>
2023-02-07 14:06:42 +0100

    Rozwiązałem zadanie `Wakacje Bajtazara (wak)' na 100 punktów

--- olimpiada_informatyczna/wakacje_bajtazara/wak.cpp
+++ olimpiada_informatyczna/wakacje_bajtazara/wak.cpp
@@ -19,10 +19,17 @@ vector<int> children[N];
 bool visited[N];
 queue<int> my_queue;
 
+constexpr int RED = 1, BLUE = 2;
 i64 red[N], blue[N];
+int red_dir[N], blue_dir[N];
 i64 W;
+int g, t;
+vector<int> path;
+int k;
 
 int main() {
+
+    vector<int> vect(100*N);
 
     ios_base::sync_with_stdio(0);
     cin.tie(0), cout.tie(0);
@@ -53,28 +60,48 @@ int main() {
     }
 
     function<void(int)> Visit = [&](int u) {
-        for(int v : children[u]) Visit(v);
+        //for(int v : children[u]) Visit(v);
         
         // compute red[u]
         red[u] = w[u];
-        for(int v : children[u])
-            red[u] = max(red[u], w[u] + blue[v]);
+        red_dir[u] = -1;
+        for(int v : children[u]) {
+            if(w[u] + blue[v] > red[u]) {
+                red[u] = w[u] + blue[v];
+                red_dir[u] = v;
+            }
+        }
 
         // compute blue[u]
         blue[u] = 0;
-        for(int v : children[u])
-            blue[u] = max(blue[u], red[v] - w[v]);
-        for(int v : children[u])
-            blue[u] += w[v];
+        blue_dir[u] = -1;
+        for(int v : children[u]) {
+            if(red[v] - w[v] > blue[u]) {
+                blue[u] = red[v] - w[v];
+                blue_dir[u] = v;
+            }
+        }
+        for(int v : children[u]) blue[u] += w[v];
     };
-    Visit(root);
+    //Visit(root);
+    int Visited[N];
+    for(int u = 1; u <= n; ++u) {
+        Visited[u] = children[u].size();
+        if(Visited[u] == 0) my_queue.push(u);
+    }
 
-    W = 0;
+    while(my_queue.size() > 0) {
+        int u = my_queue.front();
+        my_queue.pop();
+        Visit(u);
+        if(u == root) continue;
+        Visited[parent[u]]--;
+        if(Visited[parent[u]] == 0) my_queue.push(parent[u]);
+    }
+
+    W = 0, g = -1, t = -1;
     for(int u = 1; u <= n; ++u) {
         //u = 2;
-
-        W = max(W, red[u]);
-        W = max(W, blue[u]);
         int v1, v2;
 
         //cout << u << " ";
@@ -83,31 +110,191 @@ int main() {
         v1 = -1, v2 = -1;
         for(int v : children[u]) if(v1 == -1 || blue[v] > blue[v1]) v1 = v;
         for(int v : children[u]) if(v != v1 && (v2 == -1 || blue[v] > blue[v2])) v2 = v;
-        if(v1 != -1 && v2 != -1) Red += blue[v1] + blue[v2];
-        W = max(W, Red);
+        if(v1 != -1) Red += blue[v1];
+        if(v2 != -1) Red += blue[v2];
+        if(Red > W) {
+            W = Red;
+            g = u;
+            t = RED;
+        }
+        
         //cout << Red << " ";
 
         i64 Blue = 0;
         v1 = -1, v2 = -1;
         for(int v : children[u]) if(v1 == -1 || red[v] - w[v] > red[v1] - w[v1]) v1 = v;
         for(int v : children[u]) if(v != v1 && (v2 == -1 || red[v] - w[v] > red[v2] - w[v2])) v2 = v;
-        if(v1 != -1 && v2 != -1) {
-            Blue += (red[v1] - w[v1]) + (red[v2] - w[v2]);
-            for(int v : children[u]) Blue += w[v];
-        }
-        //cout << v1 << " " << v2 << "\n";
+        if(v1 != -1) Blue += red[v1] - w[v1];
+        if(v2 != -1) Blue += red[v2] - w[v2];
+        for(int v : children[u]) Blue += w[v];
         if(u != root) Blue += w[parent[u]];
-        W = max(W, Blue);
+        //W = max(W, Blue);
+        if(Blue > W) {
+            //cout << Blue << " ";
+            W = Blue;
+            g = u;
+            t = BLUE;
+        }
+        
         //cout << Blue << " ";
         //cout << W << "\n";
 
         //break;
     }
+    //cout << "\n";
     
     //for(int u = 1; u <= n; ++u) cout << red[u] << " "; cout << "\n";
     //for(int u = 1; u <= n; ++u) cout << blue[u] << " "; cout << "\n";
 
-    cout << W << "\n";
+    if(t == RED) {
+        path.push_back(g);
+        int v1, v2;
+
+        v1 = -1, v2 = -1;
+        for(int v : children[g]) if(v1 == -1 || blue[v] > blue[v1]) v1 = v;
+        for(int v : children[g]) if(v != v1 && (v2 == -1 || blue[v] > blue[v2])) v2 = v;
+
+        if(v1 != -1) {
+                int u = v1, next_u;
+
+            while(true) {
+
+                // you are blue now
+                next_u = blue_dir[u];
+                for(int v : children[u]) {
+                    if(v == next_u) continue;
+                    path.push_back(u);
+                    path.push_back(v);
+                }
+                if(next_u == -1) break;
+
+                path.push_back(u);
+                path.push_back(next_u);
+
+                u = next_u;
+
+                // you are red now
+                next_u = red_dir[u];
+
+                if(next_u == -1) break;
+                u = next_u;
+            }
+        }
+
+        reverse(path.begin(), path.end());
+
+        if(v2 != -1) {
+                int u = v2, next_u;
+
+            while(true) {
+
+                // you are blue now
+                next_u = blue_dir[u];
+                for(int v : children[u]) {
+                    if(v == next_u) continue;
+                    path.push_back(u);
+                    path.push_back(v);
+                }
+                if(next_u == -1) break;
+
+                path.push_back(u);
+                path.push_back(next_u);
+
+                u = next_u;
+
+                // you are red now
+                next_u = red_dir[u];
+
+                if(next_u == -1) break;
+                u = next_u;
+            }
+        }
+    } else if(t == BLUE) {
+        int v1, v2;
+        v1 = -1, v2 = -1;
+        for(int v : children[g]) if(v1 == -1 || red[v] - w[v] > red[v1] - w[v1]) v1 = v;
+        for(int v : children[g]) if(v != v1 && (v2 == -1 || red[v] - w[v] > red[v2] - w[v2])) v2 = v;
+
+        for(int u : children[g]) {
+            if(u == v1 || u == v2) continue;
+            path.push_back(g);
+            path.push_back(u);
+        }
+        if(g != root) {
+            path.push_back(g);
+            path.push_back(parent[g]);
+        }
+
+        if(v1 != -1) {
+            path.push_back(g);
+            path.push_back(v1);
+
+            int u = v1, next_u;
+            
+            while(true) {
+
+                // you are red now
+                next_u = red_dir[u];
+
+                if(next_u == -1) break;
+                u = next_u;
+
+                // you are blue now
+                next_u = blue_dir[u];
+                for(int v : children[u]) {
+                    if(v == next_u) continue;
+                    path.push_back(u);
+                    path.push_back(v);
+                }
+                if(next_u == -1) break;
+
+                path.push_back(u);
+                path.push_back(next_u);
+
+                u = next_u;
+            }
+        }
+
+        reverse(path.begin(), path.end());
+        path.pop_back();
+
+        if(v2 != -1) {
+            path.push_back(g);
+            path.push_back(v2);
+
+            int u = v2, next_u;
+            
+            while(true) {
+
+                // you are red now
+                next_u = red_dir[u];
+
+                if(next_u == -1) break;
+                u = next_u;
+
+                // you are blue now
+                next_u = blue_dir[u];
+                for(int v : children[u]) {
+                    if(v == next_u) continue;
+                    path.push_back(u);
+                    path.push_back(v);
+                }
+
+                if(next_u == -1) break;
+
+                path.push_back(u);
+                path.push_back(next_u);
+
+                u = next_u;
+            }
+        }
+    }
+
+    k = path.size() / 2 + 1;
+
+    cout << W << "\n" << k << "\n";
+    for(int u : path) cout << u << " ";
+    cout << "\n";
 
     return 0;
 }
