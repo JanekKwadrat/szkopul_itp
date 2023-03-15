a1577457b1405373c137d6320094269db01a34ba
Jan Zakrzewski <janek.kwadrat@gmail.com>
2023-03-15 10:56:51 +0100

    Rozwiązałem zadanie 'Parada (par)' na 100 punktów.

--- olimpiada_informatyczna/parada/par.cpp
+++ olimpiada_informatyczna/parada/par.cpp
@@ -5,7 +5,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-constexpr int N = 2e5+20;
+constexpr int N = 200'000 + 50;
 int n;
 vector<int> neighbors[N];
 
@@ -14,24 +14,25 @@ int parent[N];
 vector<int> children[N];
 
 bool visited[N];
+int licznik[N];
 queue<int> Queue;
 
-int best_path[N]; // best_path[u] = jaka jest najlepsza ścieżka zaczynająca się w wierzchołka u i schodząca tylko w dół
-int answer; // ostateczna odpowiedź tutaj będzie
+int best[N]; // best[u] = ile bramek byłoby potrzebnych na najlepszej trasie kończącej się w wierzchołku u, w poddrzewie u (nie liczymy bramki między u a jego rodzicem)
 
-/*
-10
-7 1
-3 8
-5 1
-8 10
-6 2
-4 2
-2 10
-1 10
-9 8
--> 5
-*/
+int ans;
+
+inline pair<int, int> max(pair<int, int> ab, int x) {
+    // pomaga w znalezieniu dwóch największych elementów
+    int a = ab.first, b = ab.second;
+    if(a < b) swap(a, b);
+    if(x > a) {
+        b = a;
+        a = x;
+    } else if(x > b) {
+        b = x;
+    }
+    return make_pair(a, b);
+}
 
 int main() {
 
@@ -39,18 +40,12 @@ int main() {
     cin.tie(0), cout.tie(0);
 
     cin >> n;
-    
-    //cout << n << "_";
-    
     for(int i = 1; i <= n-1; ++i) {
         int u, v;
         cin >> u >> v;
-        //cout << u << "_" << v << "_";
         neighbors[u].push_back(v);
         neighbors[v].push_back(u);
     }
-
-    //cout << "\n";
 
     for(int u = 1; u <= n; ++u) visited[u] = false;
     root = 1;
@@ -68,32 +63,43 @@ int main() {
         }
     }
 
-    function<void(int)> Visit = [&](int u) {
-        for(int v : children[u]) Visit(v);
-        best_path[u] = 0;
-        for(int v : children[u])
-            best_path[u] = max(best_path[u], best_path[v] - 1);
-        best_path[u] += children[u].size();
-    };
-    Visit(root);
-
-    //for(int u = 1; u <= n; ++u) cout << u << " "; cout << "\n";
-    //for(int u = 1; u <= n; ++u) cout << best_path[u] << " "; cout << "\n";
-
-    answer = 0;
     for(int u = 1; u <= n; ++u) {
-        int v1 = -1, v2 = -1;
-        int answer1 = children[u].size();
-        if(u != root) answer1++;
-        for(int v : children[u]) {
-            if(v1 == -1 || best_path[v] > best_path[v1]) v1 = v;
-            else if(v2 == -1 || best_path[v] > best_path[v2]) v2 = v;
-        }
-        answer1 += best_path[v1] - 1;
-        if(v2 != -1) answer1 += max(0, best_path[v2] - 1);
-        answer = max(answer, answer1);
+        licznik[u] = children[u].size();
+        if(licznik[u] == 0) Queue.push(u);
     }
-    cout << answer << "\n";
-    
+
+    while(Queue.size() > 0) {
+        int u = Queue.front();
+        Queue.pop();
+        if(u != root) {
+            licznik[parent[u]]--;
+            if(licznik[parent[u]] == 0)
+                Queue.push(parent[u]);
+        }
+        
+        best[u] = children[u].size();
+        for(int v : children[u]) {
+            best[u] = max(best[u], best[v] + (int) children[u].size() - 1);
+        }
+    }
+
+    ans = 0;
+    for(int u = 1; u <= n; ++u) {
+        pair<int, int> odp = make_pair(-1, -1);
+        for(int v : children[u]) odp = max(odp, best[v]);
+        if(odp.first < odp.second) swap(odp.first, odp.second);
+        int ans1 = children[u].size();
+        if(odp.first != -1) ans1 += odp.first - 1;
+        else continue;
+        if(odp.second != -1) ans1 += max(odp.second - 1, 0);
+        if(u != root) ans1++;
+        ans = max(ans, ans1);
+    }
+
+    // for(int u = 1; u <= n; ++u) cout << u << " "; cout << "\n";
+    // for(int u = 1; u <= n; ++u) cout << best[u] << " "; cout << "\n";
+
+    cout << ans << "\n";
+
     return 0;
 }
